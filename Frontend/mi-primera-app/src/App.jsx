e01e1414dbf34934d3d7ba1c8a3b60bb205a5e73
Diego Agustin Ramirez Martinez <diegoagustin.ramirez@globant.com>
2021-08-09 09:00:44 -0300

    header

--- Frontend/mi-primera-app/src/App.jsx
+++ Frontend/mi-primera-app/src/App.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
+  //document.getelementbyid("agregar").style.display="block";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>
+          <h1>Api Peliculas</h1>
+          <button id="agregar">agregar</button>
+          <button id="mostrar">mostrar todas las Peliculas</button>
+          <button id="filtar">Filtrar Peliculas por id</button>
+        </div>
       </header>
     </div>
   );
 }
+
 
 export default App;
