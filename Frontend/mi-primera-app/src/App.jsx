86b1c3ddeea9a7130d478dc3e7d58e6a21d3efbe
Diego Agustin Ramirez Martinez <diegoagustin.ramirez@globant.com>
2021-08-09 11:27:34 -0300

    add form

--- Frontend/mi-primera-app/src/App.jsx
+++ Frontend/mi-primera-app/src/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import './App.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   //document.getelementbyid("agregar").style.display="block";
   return (
     <div className="App">
-      <header>
-        <div>
-          <h1>Api Peliculas</h1>
-          <button id="agregar">agregar</button>
-          <button id="mostrar">mostrar todas las Peliculas</button>
-          <button id="filtar">Filtrar Peliculas por id</button>
+      <header id="main-header">
+        <div className="contenedor-menu">
+          <h1 id="titulo">Api Peliculas</h1>
+          <button id="agregar" type="submit">agregar</button>
+          <button id="mostrar" type="submit">mostrar todas las Peliculas</button>
+          <button id="filtar"  type="submit">Filtrar Peliculas por id</button>
         </div>
+        <form id="form-agendaa"action="">
+          <h3>Titulo de la Pelicula</h3>
+          <input type="text"/>
+          <h3>link de la imagen</h3>
+          <input type="text"/>
+          <button id="botons" type="submit">Agregar</button>
+        </form>
       </header>
     </div>
   );
