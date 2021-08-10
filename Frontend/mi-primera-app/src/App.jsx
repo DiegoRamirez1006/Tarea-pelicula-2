147854d5a44f1dffb7c81c06a9a3e7a3f9301c2e
Diego Agustin Ramirez Martinez <diegoagustin.ramirez@globant.com>
2021-08-10 09:26:04 -0300

    add css

--- Frontend/mi-primera-app/src/App.jsx
+++ Frontend/mi-primera-app/src/App.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 
 function App() {
   //document.getelementbyid("agregar").style.display="block";
@@ -10,21 +9,35 @@ function App() {
       <header id="main-header">
         <div className="contenedor-menu">
           <h1 id="titulo">Api Peliculas</h1>
-          <button id="agregar" type="submit">agregar</button>
-          <button id="mostrar" type="submit">mostrar todas las Peliculas</button>
-          <button id="filtar"  type="submit">Filtrar Peliculas por id</button>
+          <button id="agregar" type="submit">
+            agregar
+          </button>
+          <button id="mostrar" type="submit">
+            mostrar todas las Peliculas
+          </button>
+          <button id="filtar" type="submit">
+            Filtrar Peliculas por id
+          </button>
         </div>
-        <form id="form-agendaa"action="">
+        <form id="form-agendaa" action="">
           <h3>Titulo de la Pelicula</h3>
-          <input type="text"/>
+          <input type="text" />
           <h3>link de la imagen</h3>
-          <input type="text"/>
-          <button id="botons" type="submit">Agregar</button>
+          <input type="text" />
+          <button id="botons" type="submit">
+            Agregar
+          </button>
+        </form>
+        <form id="form-agendaaa" action="">
+          <h3>Filtrado por id</h3>
+          <input type="text" />
+          <button id="botonss" type="submit">
+            Agregar
+          </button>
         </form>
       </header>
     </div>
   );
 }
-
 
 export default App;
